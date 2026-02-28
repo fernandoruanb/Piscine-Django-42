8d4ea462f96950aa000ca9dcd39b29f5e66e26d5
Fernando Ruan <fernandosingaporefuture@gmail.com>
2026-02-27 21:47:30 -0300

    ex00 updated

--- django-0_initiation/ex00/myawesomescript.sh
+++ django-0_initiation/ex00/myawesomescript.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-'''
+# -e -> errexit -> if something does not work, fail immediately
+# -u -> unset -> if not exists fail
+
+set -eu
+
+die() { printf '%s\n' "Error: $*" >&2; exit 1; }
+
+# Valid commentaries in sh shell using heredoc
+
+: <<'DOC'
 	That script receives an URL from BitLy and identify the real target consulting
 	directly the BitLy server and get the Location header
-'''
+DOC
 
-output=$(curl -sI $1 | grep Location | cut -d ' ' -f 2)
+# $# indicates the quantity of arguments
+
+[ "$#" -ne 1 ] && die "usage: $0 <url>"
+
+command -v curl > /dev/null 2>&1 || die "curl not found"
+
+output=$(curl -sI "$1" | grep -E '^Location:[[:space:]]' | cut -d ' ' -f 2)
 
 echo "$output"
